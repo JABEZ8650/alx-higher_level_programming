2118d3d29d6390a893ee58d50b4d6dc6bc45bc15
yabets muhaba <eyumuhaba@gmail.com>
2022-11-30 10:01:49 -0800

    new

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-        """ Returns True if value has been correctly printed
+    """ Returns True if value has been correctly printed
 (it means the value is an integer)"""
     try:
         print("{:d}".format(value))
